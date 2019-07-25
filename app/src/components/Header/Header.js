a0fd41bf5c9a2d7a54718a363a6cf53a3a8b114d
Mike Z <mzapata2991@gmail.com>
2019-07-24 21:29:10 -0500

    main files created, paths created, characters with images added

---
+++ app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+   <header className="header">
+   	<h1>Clicky Game!</h1>
+   	<h2>Click on image to score points. Do not click the same image twice or your lose the game.</h2>
+   </header>
+
+);
+
+export default Header;
